0e78378a64ed17cddc2d1fa82e8aa47a24bdb11a
john <jukebox42@gmail.com>
2024-04-14 21:19:25 -0400

    hmm

--- src/actions/host.ts
+++ src/actions/host.ts
@@ -4,42 +4,52 @@ import { v4 as uuidv4 } from "uuid";
 
 import { Actions, User, Votes } from ".";
 
-const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID as string,
-  key: process.env.PUSHER_APP_KEY as string,
-  secret: process.env.PUSHER_APP_SECRET as string,
-  cluster: process.env.PUSHER_APP_CLUSTER as string,
-  encrypted: true
-});
+const createPusher = () => {
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID as string,
+    key: process.env.PUSHER_APP_KEY as string,
+    secret: process.env.PUSHER_APP_SECRET as string,
+    cluster: process.env.PUSHER_APP_CLUSTER as string,
+    encrypted: true
+  });
+  return pusher;
+}
 
 export const create = async () => {
   return uuidv4();
 }
 
 export const joined = async (id: string, success: boolean, user?: User, users?: User[], votes?: Votes, isReview?: boolean) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_JOINED, { success, user, users, votes, isReview });
 }
 
 export const sendUsers = async (id: string, users: User[]) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_USERS, users);
 }
 
 export const check = async (id: string) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_CHECK, null);
 }
 
 export const voted = async (id: string, success: boolean, value?: string) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_VOTED, { success, value });
 }
 
 export const stopVoting = async (id: string, votes: Votes) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_STOP_VOTING, votes);
 }
 
 export const resetVoting = async (id: string) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_RESET_VOTING, null);
 }
 
 export const end = async (id: string) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.HOST_END_SESSION, null);
 }
--- src/actions/user.ts
+++ src/actions/user.ts
@@ -3,16 +3,20 @@ import Pusher from "pusher";
 import { v4 as uuidv4 } from "uuid";
 import { Actions, options } from ".";
 
-const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID as string,
-  key: process.env.PUSHER_APP_KEY as string,
-  secret: process.env.PUSHER_APP_SECRET as string,
-  cluster: process.env.PUSHER_APP_CLUSTER as string,
-  encrypted: true
-});
+const createPusher = () => {
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID as string,
+    key: process.env.PUSHER_APP_KEY as string,
+    secret: process.env.PUSHER_APP_SECRET as string,
+    cluster: process.env.PUSHER_APP_CLUSTER as string,
+    encrypted: true
+  });
+  return pusher;
+}
 
 export const join = async (id: string, name: string, isObserver = false) => {
   const userId = uuidv4();
+  const pusher = createPusher();
   pusher.trigger(id, Actions.USER_JOIN, { id: userId, name, isObserver });
   return userId;
 }
@@ -21,14 +25,17 @@ export const vote = async (id: string, userId: string, value: string) => {
   if (!options.find(o => o === value)) {
     return;
   }
+  const pusher = createPusher();
   pusher.trigger(id, Actions.USER_VOTE, { userId, value });
 }
 
 export const checked = async (id: string, userId: string) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.USER_CHECKED, { userId });
 }
 
 export const leave = async (id: string, userId: string) => {
+  const pusher = createPusher();
   pusher.trigger(id, Actions.USER_LEAVE, { userId });
 }
 
